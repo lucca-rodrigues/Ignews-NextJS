38aae908938fa6d7a003c7a942a2a38d700e79ef
Lucca Rodrigues <contato.luccarodrigues@gmail.com>
2021-06-08 22:27:15 -0300

    home page

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,14 +1,21 @@
 
+import SignInButton from '../SignInButton';
+
 import styles from './styles.module.scss';
 
 export default function Header() {
     return (
         <header className={styles.headerContainer}>
             <div className={styles.headerContent}>
+                <img src="/images/logo.svg" alt="ig.news" />
+
                 <nav>
                     <a>Home</a>
+
                     <a>Posts</a>
                 </nav>
+
+                <SignInButton />
             </div>
         </header>
     );
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,37 @@
 import Head from 'next/head';
+import styles from './home.module.scss';
+
+// interface HomeProps {
+//   product: {
+//     priceId: string;
+//     amount: string;
+//   };
+// }
 
 export default function Home() {
   return (
     <>
       <Head>
-        <title>Início | ig.news</title>
+        <title>Home | ig.news</title>
       </Head>
-      <h1>Hello <span>Home</span></h1>
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👏 Hey, welcome</span>
+
+          <h1>
+            News about the <span>React</span> world.
+          </h1>
+
+          <p>
+            Get access to all the publications <br />
+            <span>for month</span>
+          </p>
+
+          {/* <SubscribeButton priceId={product.priceId} /> */}
+        </section>
+
+        <img src="/images/avatar.svg" alt="Girl coding" />
+      </main>
     </>
-  )
+  );
 }
